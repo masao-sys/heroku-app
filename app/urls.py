3a320f814dc2e3f913b81151934eecf2897c4a74
Takeuchi <masao@MacBook-Ari-Masao.local>
2024-06-01 16:12:10 +0900

    stripe機能を実装

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path, include
-from app.views import top
+from app.views import top, credit
 
 urlpatterns = [
     path('', top.TopView.as_view(), name='top'),
     path('accounts/', include('allauth.urls')),
+
+    path('credit/register/', credit.CreditRegisterView.as_view(), name='credit-register'),
+    path('credit/update/', credit.CreditUpdateView.as_view(), name='credit-update'),
+    path('subscription/cancel/', credit.SubscriptionCancelView.as_view(), name='subscription-cancel'),
 ]
