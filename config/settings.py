6ffe7d921c3ad88269a366ece01b08797623e4a2
Takeuchi <masao@MacBook-Ari-Masao.local>
2024-08-01 19:42:52 +0900

    メールの再修正

--- config/settings.py
+++ config/settings.py
@@ -246,12 +246,7 @@ ACCOUNT_EMAIL_REQUIRED = True
 
 # メール送信設定
 if DEBUG:
-    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_HOST = env('EMAIL_HOST')
-    EMAIL_PORT = env('EMAIL_PORT')
-    EMAIL_USE_TLS = True
-    EMAIL_HOST_USER = env('EMAIL_HOST_USER')
-    EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_HOST = env('EMAIL_HOST')
